bad41168191f3cd7575ac3b4bf1a66669f998db1
Jinsoo Jeon <proddler@naver.com>
2022-08-08 17:26:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 import pymysql 
 
-conn = pymysql.connect(host='database-1.cceupqtdw2si.ap-northeast-2.rds.amazonaws.com', user='admin', password='', db='goorm', charset='utf8', port=3306) 
+conn = pymysql.connect(host='database-1.cceupqtdw2si.ap-northeast-2.rds.amazonaws.com', user='admin', password='GOORM0808', db='goorm', charset='utf8', port=3306) 
 cursor = conn.cursor() 
 
 sql = '''CREATE TABLE user ( 
