23799cd7016f5c4ea6c6dc7480516e8dc38ea64b
Jinsoo Jeon <proddler@naver.com>
2022-08-08 21:46:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
 import pymysql 
 
-conn = pymysql.connect(host='database-1.cceupqtdw2si.ap-northeast-2.rds.amazonaws.com', user='admin', password='GOORM0808', db='goorm', charset='utf8', port=3306) 
+conn = pymysql.connect(host='database-1.cceupqtdw2si.ap-northeast-2.rds.amazonaws.com', user='admin', password='GOORM0808', charset='utf8', port=3306) 
 
-''' 
-cursor = db.cursor() 
 
-sql = """
-    INSERT INTO user VALUES('aa','aa');
-"""
+cursor = conn.cursor() 
+
+sql = "CREATE DATABASE exercise"
+
 
 cursor.execute(sql)
 
-db.commit()
-
-db.close()
-
-
-
+conn.commit() 
+conn.close()
