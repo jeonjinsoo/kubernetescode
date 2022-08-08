211f643bb70ee090e6c1c41172431f1b767de99b
Jinsoo Jeon <proddler@naver.com>
2022-08-08 20:29:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
-app = Flask(__name__)
+import pymysql 
 
-@app.route('/')
-def hello_world():
-    return 'Please subscribe, like, and comment on this video, TY!!!'
+conn = pymysql.connect(host='http://database-1.cceupqtdw2si.ap-northeast-2.rds.amazonaws.com', user='admin', password='GOORM0808', db='goorm', charset='utf8') 
+cursor = conn.cursor() 
+
+sql = '''CREATE TABLE user ( 
+id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+email varchar(255), 
+department varchar(255) 
+) 
+''' 
+
+cursor.execute(sql) 
+
+conn.commit() 
+conn.close()
+
 
