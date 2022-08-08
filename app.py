b1cfc8c3f5d42a0918d853afbb6fcfa402df0228
Jinsoo Jeon <proddler@naver.com>
2022-08-08 21:40:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 import pymysql 
 
 conn = pymysql.connect(host='database-1.cceupqtdw2si.ap-northeast-2.rds.amazonaws.com', user='admin', password='GOORM0808', db='goorm', charset='utf8', port=3306) 
-cursor = conn.cursor() 
 
-sql = '''CREATE TABLE user ( 
-id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-email varchar(255), 
-department varchar(255) 
-) 
 ''' 
+cursor = db.cursor() 
 
-cursor.execute(sql) 
+sql = """
+    INSERT INTO user VALUES('aa','aa');
+"""
 
-conn.commit() 
-conn.close()
+cursor.execute(sql)
+
+db.commit()
+
+db.close()
+
 
 
