8faece56e013ec621944b966ff6d3d9e76e980b0
Jinsoo Jeon <proddler@naver.com>
2022-08-08 15:18:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,79 +1,20 @@
-from flask import Flask, session, render_template, redirect, request, url_for
-from flask_mysqldb import MySQL
-mysql=MySQL()
-app = Flask(__name__)
 
-app.config['MYSQL_HOST']='http://database-1.cceupqtdw2si.ap-northeast-2.rds.amazonaws.com/'
+import pymysql
 
-app.config['MYSQL_USER']='admin'
+ 
+import pymysql 
 
-app.config['MYSQL_PASSWORD']='GOORM0808'
+conn = pymysql.connect(host='http://database-1.cceupqtdw2si.ap-northeast-2.rds.amazonaws.com', user='root', password='GOORM0808', db='goorm', charset='utf8') 
+cursor = conn.cursor() 
 
-app.config['MYSQL_DB']='goorm'
+sql = '''CREATE TABLE user ( 
+id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+email varchar(255), 
+department varchar(255) 
+) 
+''' 
 
-app.secret_key = "ABCDEFG"
+cursor.execute(sql) 
 
-mysql.init_app(app)
- 
-@app.route('/', methods=['GET', 'POST'])
-def main():
-    error = None
- 
-    if request.method == 'POST':
-        id = request.form['id']
-        pw = request.form['pw']
- 
-        conn = mysql.connect()
-        cursor = conn.cursor()
-        sql = "SELECT id FROM users WHERE id = %s AND pw = %s"
-        value = (id, pw)
-        cursor.execute("set names utf8")
-        cursor.execute(sql, value)
- 
-        data = cursor.fetchall()
-        cursor.close()
-        conn.close()
- 
-        for row in data:
-            data = row[0]
- 
-        if data:
-            session['login_user'] = id
-            return redirect(url_for('home'))
-        else:
-            error = 'invalid input data detected !'
-    return render_template('main.html', error = error)
- 
- 
-@app.route('/register.html', methods=['GET', 'POST'])
-def register():
-    error = None
-    if request.method == 'POST':
-        id = request.form['regi_id']
-        pw = request.form['regi_pw']
- 
-        conn = mysql.connect()
-        cursor = conn.cursor()
- 
-        sql = "INSERT INTO users VALUES ('%s', '%s')" % (id, pw)
-        cursor.execute(sql)
- 
-        data = cursor.fetchall()
- 
-        if not data:
-            conn.commit()
-            return redirect(url_for('main'))
-        else:
-            conn.rollback()
-            return "Register Failed"
- 
-        cursor.close()
-        conn.close()
-    return render_template('register.html', error=error)
- 
-@app.route('/home.html', methods=['GET', 'POST'])
-def home():
-    error = None
-    id = session['login_user']
-    return render_template('home.html', error=error, name=id)
- 
+conn.commit() 
+conn.close() 
