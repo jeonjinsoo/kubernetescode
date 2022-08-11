b5d02c4e8ecb88bc02b299789ec0214d64cc42b2
Jinsoo Jeon <proddler@naver.com>
2022-08-11 20:35:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pymysql 
 
-conn = pymysql.connect(host='database-1.cceupqtdw2si.ap-northeast-2.rds.amazonaws.com', user='admin', password==, charset='utf8', port=3306) 
+conn = pymysql.connect(host='database-1.cceupqtdw2si.ap-northeast-2.rds.amazonaws.com', user='admin', password='goorm0808'=, charset='utf8', port=3306) 
 
 
 cursor = conn.cursor() 
